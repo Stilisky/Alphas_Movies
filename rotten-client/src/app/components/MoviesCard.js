6af90f46bd2252e0ea6255fe8c96a7312a1137b4
Bitsmith01 <esperat.dimon@epitech.eu>
2023-09-06 18:05:38 +0100

    add movies to categories function

--- rotten-client/src/app/components/MoviesCard.js
+++ rotten-client/src/app/components/MoviesCard.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import Image from 'next/image'
-import wallpaper from '../../../public/Images/desktop-wallpaper-spiderman-hollywood-movie-thumbnail.jpg'
+import React from 'react';
+import Image from 'next/image';
+import wallpaper from '../../../public/Images/desktop-wallpaper-spiderman-hollywood-movie-thumbnail.jpg';
 
-const MoviesCard = () => {
+const MoviesCard = ({ data }) => {
   return (
     <div className='card my-5 flex-shrink-0'>
       <div className='imgbox'>
         <Image src={wallpaper} alt='spider' className='image' />
       </div>
       <div className='info'>
-        <h1 className='text-2xl md:text-3xl'><strong>Title</strong></h1>
+        <h1 className='text-2xl md:text-3xl'>
+          <strong>{data.title}</strong>
+        </h1>
         <div className='flex space-x-2'>
-          <button type="button" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-black bg-[#1ce783] rounded-lg w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <button
+            type="button"
+            className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-black bg-[#1ce783] rounded-lg w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
             Action
           </button>
-          <button type="button" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-black bg-[#1ce783] rounded-lg w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <button
+            type="button"
+            className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-black bg-[#1ce783] rounded-lg w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
             See more
           </button>
         </div>
-        <p className='text-white'>
-          lorem chbkhds csjcvsc sbd c hck scskchc sdcsjvc c ckschd csc sjc js cl: ccls :csdch scl dhslc shc schdscs
-        </p>
-
+        <p className='text-white'>{data.description}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MoviesCard
+export default MoviesCard;
--- rotten-client/src/app/components/MoviesLists.js
+++ rotten-client/src/app/components/MoviesLists.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import MoviesCard from './MoviesCard'
+import React from 'react';
+import MoviesCard from './MoviesCard';
 
-const MoviesLists = ({text}) => {
+const MoviesLists = ({ text, data }) => {
   return (
     <div className='px-4 md:px-12 mt-4'>
-        <div>
-            <p className='text-white text-md md:text-xl lg:text-2xl font-semibold uppercase tracking'>
-                {text}
-            </p>
-            <div className='text-white flex space-x-3 space-y-8 overflow-hidden overflow-x-scroll example1'>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-            </div>
+      <div>
+        <p className='text-white text-md md:text-xl lg:text-2xl font-semibold uppercase tracking'>
+          {text}
+        </p>
+        <div className='text-white flex space-x-3 space-y-8 overflow-hidden overflow-x-scroll example1'>
+          {data.map((movie) => (
+            <MoviesCard key={movie._id} data={movie} />
+          ))}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MoviesLists
+export default MoviesLists;
