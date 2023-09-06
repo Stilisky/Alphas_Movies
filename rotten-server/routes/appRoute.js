09ca92c4da445d1b67a3c0d289f66bf1fc2c58ac
stilisky <setonde.hounnouvi@epitech.eu>
2023-09-06 15:17:17 +0100

    Added app controller

--- rotten-server/routes/appRoute.js
+++ rotten-server/routes/appRoute.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const {categoryToMovie, commentToMovie, favoriteToUser, genreToMovies, likeToMovie, movieToCategory, moviesToGenre} = require('../controllers/appController')
+
+router.route('/category/:catid/movie/:movieid').get(categoryToMovie)
+router.route('/comment/:commentid/movie/:movieid').get(commentToMovie)
+router.route('/favorite/:userid/movie/:movieid').get(favoriteToUser)
+router.route('/genre/:genreid/movie/:movieid').get(genreToMovies)
+router.route('')
 
 
 module.exports = router;
