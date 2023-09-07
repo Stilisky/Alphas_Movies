73eecd576d44ec9c2112ca1381a86ddf1f7fcd56
Bitsmith01 <esperat.dimon@epitech.eu>
2023-09-07 13:55:17 +0100

    comment function update

--- rotten-client/src/app/components/modal.js
+++ rotten-client/src/app/components/modal.js
@@ -70,13 +70,13 @@ const Mymodal = ({ isVisible, onClose, movieId }) => {
           </div>
           <label htmlFor="chat" className="sr-only">Your comment</label>
           <div className="flex items-center px-3 py-2 rounded-lg space-x-2">
-            <button type="button" className="inline-flex justify-center p-2 text-gray-500 border rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+            <button id='addTofavorite' type="button" className="inline-flex justify-center p-2 text-gray-500 border rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
               </svg>
               <span className="sr-only">Upload image</span>
             </button>
-            <button type="button" className="p-2 text-gray-500 border rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+            <button id='like' type="button" className="p-2 text-gray-500 border rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
               </svg>
